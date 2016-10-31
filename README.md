e7ab1d87fd885675c851e2930af0e253ebed02f1
Christine Lovett <clovett07@gmail.com>
2016-10-31 15:52:01 -0400

    Added mongo heroku

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hackerwall_deployment
+
+##export MONGOLAB_URI=mongodb:"//clovett:duders12@ds139847.mlab.com:39847/hackerwall"
---
+++ _solution/index.js
@@ -0,0 +1,106 @@
+// All our requires/dependencies
+ var express = require('express')
+ var bodyParser = require('body-parser')
+ var hbs = require('express-handlebars')
+ var mongoose = require('mongoose')
+
+
+// Here we find an appropriate database to connect to, defaulting to
+ // localhost if we don't find one.
+ var uristring =
+ process.env.MONGOLAB_URI ||
+ process.env.MONGOHQ_URL ||
+ 'mongodb://localhost/HelloMongoose';
+
+ // The http server will listen to an appropriate port, or default to
+ // port 5000.
+ var theport = process.env.PORT || 3000;
+
+ // Makes connection asynchronously.  Mongoose will queue up database
+ // operations and release them when the connection is complete.
+ mongoose.connect(uristring, function (err, res) {
+   if (err) {
+   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+   } else {
+   console.log ('Succeeded connected to: ' + uristring);
+   }
+ });
+
+ // Including our Models
+ var Post = require('./models/posts')
+
+ // Creating our Application
+ var app = express()
+
+ // Registering and using our template engine
+ app.engine('handlebars', hbs({defaultLayout: 'main'}))
+ app.set('view engine', 'handlebars')
+
+ // Serving static files (like css)
+ app.use(express.static('public'));
+
+ // Use Body Parser
+ app.use(bodyParser.urlencoded({extended: true}))
+
+ // Routes
+ app.get('/', function( req , res ) {
+
+   Post.find({}, function( err, posts ) {
+
+     res.render('index', { posts: posts })
+
+   })
+
+ })
+
+ app.get('/posts/:id', function( req, res ) {
+
+   Post.findById( req.params.id, function( err, post ){
+
+     res.render( 'posts/post', { post: post } )
+
+   })
+
+ })
+
+ app.post('/posts/:id', function( req, res ) {
+
+   console.log( req.body )
+
+   Post.findById( req.params.id, function( err, post ){
+
+     post.comments.push( req.body )
+     post.save()
+
+     res.render( 'posts/post', { post: post } )
+
+   })
+
+ })
+
+ app.get('/posts/new', function( req , res ) {
+
+   res.render('posts/new')
+
+ })
+
+ app.post('/posts', function( req , res ) {
+
+   var newPost = new Post({
+     postUrl: req.body.postUrl,
+     author: req.body.author,
+     title: req.body.title,
+     description: req.body.description
+   })
+
+   newPost.save()
+
+   res.redirect('/')
+
+ })
+
+ app.listen( 3000, function() {
+
+   console.log( 'listening on 3000' )
+
+ })
